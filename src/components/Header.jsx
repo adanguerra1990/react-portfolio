a7edde58bac5f66851b1bc2a6a324e7f0d755ed1
adanguerra1990 <aj90guerra@gmail.com>
2024-05-20 17:07:07 -0400

    Header y Nav Menu

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { RiCloseLine, RiMenuLine } from '@remixicon/react'
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const navigationLinks = ['Inicio', 'Acerca de Mí', 'Proyectos', 'Contacto']
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+
+        console.log('muetra el menu', isOpen ? 'cerrado' : 'abierto')
+    }
+    const showMenu = isOpen ? 'show-menu' : ''
+
+    return (
+        <header className='header' id='header'>
+            <nav className='nav container'>
+                <a href="#" className="nav__logo">
+                    ADAN <span>GUERRA</span>
+                </a>
+
+                <div className={`nav__menu ${showMenu}`} id='nav-menu'>
+                    <ul className='nav__list'>
+                        {navigationLinks.map((link, index) => (
+                            <li className='nav__item' key={index}>
+                                <a href='#' className='nav__link active-link'>{link}</a>
+                            </li>
+                        ))}
+                    </ul>
+
+                    {/* close button */}
+                    <div className='nav__close' id='nav-close' onClick={toggleMenu}>
+                        <RiCloseLine />
+                    </div>
+                </div>
+
+                {/* toggle button */}
+                <div className='nav__toggle' id='nav-toggle' onClick={toggleMenu}>
+                    <RiMenuLine />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
