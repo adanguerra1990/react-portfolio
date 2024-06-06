e28722bdb806f9f4a674e6f63d5f08feb4345e38
adanguerra1990 <aj90guerra@gmail.com>
2024-06-06 16:15:17 -0400

    Component ScrollUp

---
+++ src/components/ScrollUp.jsx
@@ -0,0 +1,28 @@
+import { RiArrowUpLine } from "@remixicon/react";
+import { useEffect, useState } from "react";
+
+const ScrollUp = () => {
+     const [isScrolled, setIsScrolled] = useState(false)
+     
+     useEffect(() => {
+          const checkScrolleTop = () => {
+               window.scrollY > 350 ? setIsScrolled(true) : setIsScrolled(false)
+          }
+
+          window.addEventListener('scroll', checkScrolleTop)
+
+          return () => window.removeEventListener('scroll', checkScrolleTop)
+     })
+
+     const getClassName = () => {
+          return `scrollup ${isScrolled  ? 'show-scroll' : ''}`
+     }
+
+     return (
+          <a href="#" className={getClassName()} id="scroll-up">
+               <RiArrowUpLine />
+          </a>
+     );
+}
+
+export default ScrollUp;
