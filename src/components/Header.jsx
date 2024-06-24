135be64564530bfb9f04203f320a33da942be577
adanguerra1990 <aj90guerra@gmail.com>
2024-06-24 14:03:42 -0400

    Implementando clase activa para el nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { RiCloseLine, RiMenuLine } from '@remixicon/react'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
+    const [activeSection, setActiveSection] = useState('')
 
-    const navigationLinks = ['Inicio', 'Acerca de Mí', 'Proyectos', 'Contacto']
+    const navigationLinks = [
+        { title: 'Inicio', href: '#home' },
+        { title: 'Acerca de Mí', href: '#about' },
+        { title: 'Proyectos', href: '#projects' },
+        { title: 'Contacto', href: '#contact' },
+    ]
+
+    useEffect(() => {
+        const checkScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', checkScroll)
+
+        return () => window.removeEventListener('scroll', checkScroll)
+    }, [])
+
+    useEffect(() => {
+        const handleScrollActive = () => {
+            const sections = document.querySelectorAll('section[id]')
+            const scrollY = window.scrollY
+
+            sections.forEach(section => {
+                const sectionHeight = section.offsetHeight
+                const sectionTop = section.offsetTop - 58
+                const sectionId = section.getAttribute('id')
+                const sectionLink = document.querySelector(`.nav__menu a[href*=${sectionId}]`)
+
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                    setActiveSection(sectionId)
+                }                       
+            });
+        }
+
+        window.addEventListener('scroll', handleScrollActive)
+        return () => window.removeEventListener('scroll', handleScrollActive)
+    }, []);
+
+    const getClassHeader = () => `header ${isScrolled ? 'blur-header' : ''}`
 
     const toggleMenu = () => {
         setIsOpen(!isOpen)
     }
+
     const showMenu = isOpen ? 'show-menu' : ''
 
     return (
-        <header className='header' id='header'>
+        <header className={getClassHeader()} id='header'>
             <nav className='nav container'>
                 <a href="#" className="nav__logo">
                     ADAN <span>GUERRA</span>
@@ -22,7 +67,10 @@ const Header = () => {
                     <ul className='nav__list'>
                         {navigationLinks.map((link, index) => (
                             <li className='nav__item' key={index}>
-                                <a href='#' className='nav__link active-link'>{link}</a>
+                                <a
+                                    href={link.href}
+                                    className={`nav__link ${activeSection === link.href.slice(1) ? 'active-link' : ''}`} onClick={toggleMenu}>{link.title}
+                                </a>
                             </li>
                         ))}
                     </ul>
